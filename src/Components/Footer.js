993072a3c72179985a926399550d273cf561c76e
learn <learn@learn04.localdomain>
2018-12-03 16:16:19 -0800

    initial front setup with bootstrap and pages

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer-Div">
+      Team Jack, 2018, copyright symbol Goes Here (:
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Components/My_task_card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class My_Task_Card extends Component {
+  render() {
+    return (
+      <div className="My_Task_Card-Div">
+      make boxes with stuff in them (:
+      </div>
+    );
+  }
+}
+
+export default My_Task_Card;
