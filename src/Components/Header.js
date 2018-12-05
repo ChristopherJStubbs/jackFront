5fc2767e4d48f712422c223980fdd9143df7df09
Evan Powell <powellevan88@gmail.com>
2018-12-05 11:13:38 -0800

    added navbar with logout button + functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
+import {Button, Navbar, Nav, NavItem } from 'react-bootstrap';
+import AuthService from '../services';
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.auth = new AuthService()
+  }
   render() {
     return (
       <div className="Header-Div">
-      NavBar Goes Here (:
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Insert Name Here</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="/">
+              Home
+            </NavItem>
+            {!this.auth.loggedIn() &&
+            <NavItem eventKey={2} href="/user/:id/tasks">
+             My Tasks
+            </NavItem>}
+            {this.auth.loggedIn() &&
+            <NavItem eventKey={3} href='/tasks'>
+              All Tasks
+            </NavItem>}
+            {this.auth.loggedIn() &&
+            <NavItem eventKey={4} href="/">
+              Edit Tasks
+            </NavItem>}
+            {this.auth.loggedIn() &&
+            <NavItem onClick={this.handleClick} eventKey={5} href="/">
+              Logout
+            </NavItem>}
+          </Nav>
+        </Navbar>
       </div>
     );
+  }
+
+  handleClick = () => {
+    this.auth.logout();
+    this.props.logout();
+
   }
 }
 
