c733d433d267c9fdf753b78cc7335ba22fdd3a70
learn <learn@learn04.localdomain>
2018-12-05 15:49:07 -0800

    MOB refactored registration form to bootstrap. got onclick working for checkboxes in reg form.

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
 import AuthService from '../services'
+import { Form, FormGroup, Col, FormControl, Button, ControlLabel, Checkbox } from 'react-bootstrap';
 
 class Registration extends Component {
   constructor(props) {
@@ -15,72 +16,75 @@ class Registration extends Component {
           first_name: '',
           last_name: '',
           phone: '',
-          home_owner: '',
-          car_owner: ''
+          home_owner: false,
+          car_owner: false
         }
       }
     }
   }
   render() {
-    console.log(this.state.form.user)
+    console.log(this.state.form.user);
+    let { email, password, first_name, last_name, phone, home_owner, car_owner } = this.state.form.user
     return (
       <div>
-        <form className="form-horizontal">
-          <fieldset>
-            <legend>Registration</legend>
-            <div className="form-group">
-              <label for="email" className="col-lg-2 control-label">Email</label>
-              <div className="col-lg-10">
-                <input onChange={this.handleChange} type="text" className="form-control" id="email" placeholder="test@example.com" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label for="password" className="col-lg-2 control-label">Password</label>
-              <div className="col-lg-10">
-                <input onChange={this.handleChange} type="password" className="form-control" id="password" placeholder="Password" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label for="first_name" className="col-lg-2 control-label">First Name</label>
-              <div className="col-lg-10">
-                <input onChange={this.handleChange} type="text" className="form-control" id="first_name" placeholder="First Name" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label for="last_name" className="col-lg-2 control-label">Last Name</label>
-              <div className="col-lg-10">
-                <input onChange={this.handleChange} type="text" className="form-control" id="last_name" placeholder="Last Name" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label for="phone" className="col-lg-2 control-label">Phone</label>
-              <div className="col-lg-10">
-                <input onChange={this.handleChange} type="text" className="form-control" id="phone" placeholder="(800)555-5555" />
-              </div>
-            </div>
-            <div className="checkbox">
-              <label>
-                <input onChange={this.handleChange} type="checkbox" /> Home?
-              </label>
-            </div>
-            <div className="checkbox">
-              <label>
-                <input onChange={this.handleChange} type="checkbox" /> Car?
-              </label>
-            </div>
-            <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
-          </fieldset>
-        </form>
+      <Form horizontal onSubmit={this.handleSubmit}>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Email
+          </Col>
+          <Col sm={10}>
+            <FormControl onChange={this.handleChange} name="email" value={email} type="email" placeholder="Email" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={10}>
+            <FormControl onChange={this.handleChange} name="password" value={password} type="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalFirstName">
+          <Col componentClass={ControlLabel} sm={2}>
+            First Name
+          </Col>
+          <Col sm={10}>
+            <FormControl onChange={this.handleChange} name="first_name" value={first_name} type="FirstName" placeholder="First Name" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalLastName">
+          <Col componentClass={ControlLabel} sm={2}>
+            Last Name
+          </Col>
+          <Col sm={10}>
+            <FormControl onChange={this.handleChange} name="last_name" value={last_name} type="LastName" placeholder="Last Name" />
+          </Col>
+        </FormGroup>
+        <FormGroup controlId="formHorizontalPhone">
+          <Col componentClass={ControlLabel} sm={2}>
+            Phone
+          </Col>
+          <Col sm={10}>
+            <FormControl onChange={this.handleChange} name="phone" value={phone} type="phone" placeholder="555-555-5555" />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+              <Checkbox onClick={() => this.handleCheck(1)} inline>Home?</Checkbox> <Checkbox onClick={() => this.handleCheck(2)} inline>Car?</Checkbox>
+        </FormGroup>
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button  type="submit">Sign In</Button>
+          </Col>
+        </FormGroup>
         {this.state.submitted && <Redirect to="/"/>}
+      </Form>
       </div>
     )
   }
 
   handleChange = (e) => {
-    e.preventDefault()
     let { form } = this.state
-    form.user[e.target.id] = e.target.value
-    console.log(this.state.form.user)
+    form.user[e.target.name] = e.target.value
     this.setState({ form })
   }
 
@@ -89,7 +93,6 @@ class Registration extends Component {
     this.auth.register(this.state.form)
     .then(status => {
       if(status.errors){
-        console.log(status.errors.status);
         this.setState({ submitted: false })
       }else{
         this.auth.sign_in(this.state.form)
@@ -98,6 +101,20 @@ class Registration extends Component {
     })
   }
 
+  handleCheck = (type) => {
+    let { form } = this.state
+    if (type == 1) {
+      form.user.home_owner = !form.user.home_owner
+      this.setState ({
+        home_owner: true
+      })
+    } else if (type == 2) {
+      form.user.car_owner = !form.user.car_owner
+      this.setState ({
+        car_owner: true
+      })
+    }
+  }
 
 }
 
