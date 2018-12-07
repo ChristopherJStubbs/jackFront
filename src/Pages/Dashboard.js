53a0f0933d710783a76875c9fa16a554add66350
Evan Powell <powellevan88@gmail.com>
2018-12-07 11:59:10 -0800

    added dashboard, have to move logic to Appjs

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
+import AuthService from '../services';
+import My_Task_Card from './Components/My_task_card';
+import { Grid, Col, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
 class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <h1>This is the dashboard</h1>
-      </div>
-    )
+  constructor(props) {
+    super(props)
+
+    this.auth = new AuthService()
+    this.state = {
+      tasks: undefined
+    }
   }
+  render() {
+    if (this.state.tasks = !undefined) {
+    return (
+      <Grid>
+       <Row>
+         <Col xs={12}>
+             <ListGroup>
+              {this.state.my_tasks.map((tasks, index) => {
+            return (
+              <ListGroupItem
+                key={tasks.id}
+                header={
+                  <Link to={`/apartments/${tasks.id}`}>
+                  <h4>
+                    <span className='tasks-category'>
+                      {tasks.category}
+                    </span> - <small className='tasks-subcategory'> {tasks.subcategory} </small>
+                    <small className='tasks-title'>
+                     {tasks.state} </small>
+                    <small className='tasks-description'>
+                     {tasks.description} </small>
+                  </h4>
+                  </Link>
+                }>
+              </ListGroupItem>
+            )
+          })}
+            </ListGroup>
+          </Col>
+        </Row>
+       </Grid>
+       )
+     }else {
+       return(
+         <div>
+         Loading your upcoming tasks...
+         </div>
+       )
+     }
+   }
+
+   componentDidMount(){
+     console.log(this.state.user.id)
+     getMyTasks(this.state.user.id)
+     .then((tasks)=> {
+       console.log(tasks);
+       this.setState({tasks})
+     }
+     )
+   }
+
 }
 
 export default Dashboard;
